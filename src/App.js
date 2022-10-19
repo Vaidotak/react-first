f59c40904a8bafc6743154b4e3a11abe420c8773
Vaidotak <pehtrauhk@gmail.com>
2022-10-19 11:35:51 +0300

    npm run watch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Čia kažkas bus.
+          čia kažkas bus.
         </p>
         <a
           className="App-link"
