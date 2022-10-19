37a5eb8ea0453e9be57860668c68889861690d67
Vaidotak <pehtrauhk@gmail.com>
2022-10-19 11:22:40 +0300

    Mažas pakeitimas

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          MOKSLAI PRASIDEDA! SPA jau sukasi ant React.
+          Čia kažkas bus.
         </p>
         <a
           className="App-link"
