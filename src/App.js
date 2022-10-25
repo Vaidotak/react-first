f725670b1b11e7c4ed7d75273b83ae66ba0a22c0
Vaidotak <pehtrauhk@gmail.com>
2022-10-25 19:16:14 +0300

    Atnaujintos nuorodos ru browser-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header/Header";
-import Navbar from "./Navbar/Navbar";
-import Profile from "./Profile/Profile";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-content">
+          <Routes>
+            <Route path='/profile' element={<Profile />}/>
+            <Route path='/dialogs' element={<Dialogs />}/>
+            <Route path='/music' element={<Music />}/>
+            <Route path='/news' element={<News />}/>
+            <Route path='/settings' element={<Settings />}/>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import s from './Navbar.module.css';
+
+const activeLink = ({isActive}) => isActive ? s.active : s.item;
+
+const Navbar =()=>{
+  
+    return(
+        <nav className={s.nav}>
+      <div>
+        <NavLink to='/profile' className = {activeLink }>Profile</NavLink>
+        </div>
+      <div>
+        <NavLink to='/dialogs' className = {activeLink }>Messages</NavLink>
+        </div>
+      <div>
+        <NavLink to='/news' className = { activeLink }>News</NavLink>
+        </div>
+      <div>
+        <NavLink to='/music' className = { activeLink }>Music</NavLink>
+        </div>
+      <div>
+        <NavLink to='settings' className = { activeLink }>Settings</NavLink>
+        </div>
+      
+      </nav>
+    )
+}
+
+export default Navbar;
